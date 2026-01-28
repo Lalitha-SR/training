d657cb09482c47815cf48bcc00741cea6db36a78
Lalitha S R <lalitsr@eu.trimblecorp.net>
2026-01-28 17:57:28 +0530

    remove lockstate

--- backend.tf
+++ backend.tf
@@ -3,9 +3,7 @@ terraform {
     bucket         = "terraform-state-test29"
     key            = "vpc/terraform.tfstate"
     region         = "us-east-1"
-    encrypt        = true
-       use_lockfile = true
-  
+    encrypt        = true 
     dynamodb_table = "terraform-locks"
   }
 }
